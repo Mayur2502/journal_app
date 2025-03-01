473b53c2ab0f44bd36e6db5b9b2626c21be5b3e9
Mayur2502 <mayurpaul170@gmail.com>
2025-03-01 22:46:19 +0530

    01-03

--- src/test/java/net/engineeringdigest/journalApp/service/UserDetailServiceImplTests.java
+++ src/test/java/net/engineeringdigest/journalApp/service/UserDetailServiceImplTests.java
@@ -1,2 +1,25 @@
-package net.engineeringdigest.journalApp.service;public class UserDetailServiceImplTests {
+package net.engineeringdigest.journalApp.service;
+
+import net.engineeringdigest.journalApp.entity.User;
+import net.engineeringdigest.journalApp.repository.UserRepository;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.mockito.Mockito.when;
+
+public class UserDetailServiceImplTests {
+
+
+    @Autowired
+    private UserDetailServiceImpl userDetailService;
+ 
+        @Mock
+        private UserRepository userRepository;
+
+        void loadUserByUsernameTests(){
+            when(userRepository.findByUserName(ArgumentMatchers.anyString())).thenReturn(User.builder().userName("ram").password("sdfsj").build());
+            userDetailService.loadUserByUsername("ram");
+        }
 }
